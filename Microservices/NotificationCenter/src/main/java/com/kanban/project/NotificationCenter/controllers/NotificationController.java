22f8d744d724b805f3a729cda4b1323e433d1ff0
Mykhailo Humeniuk <mukhailo.humenuik@code-seek.com>
2026-06-15 13:21:00 +0300

    Small fix

--- Microservices/NotificationCenter/src/main/java/com/kanban/project/NotificationCenter/controllers/NotificationController.java
+++ Microservices/NotificationCenter/src/main/java/com/kanban/project/NotificationCenter/controllers/NotificationController.java
@@ -1,5 +1,6 @@
 package com.kanban.project.NotificationCenter.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,14 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/internal/notifications")
 public class NotificationController {
-
     private final SimpMessagingTemplate messagingTemplate;
-
-    public NotificationController(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
 
     @PostMapping("/boards/{boardId}")
     public void broadcastBoardUpdate(@PathVariable Long boardId, @RequestBody Object payload) {
